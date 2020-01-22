47b63710c5cdaa22882464b949b5fe7566bad2a1
Bence Monus <crome@moronia.hu>
2020-01-22 15:58:39 +0000

    handle request/parsing errors

--- backend/spec/fetcher_spec.cr
+++ backend/spec/fetcher_spec.cr
@@ -7,20 +7,39 @@ class ExampleEntity
 end
 
 describe Fetcher do
+  before_each do
+    WebMock.reset
+  end
+
   describe "#fetch" do
-    before_each do
-      WebMock.stub(:get, "https://test.com").
-        to_return(body: %{{"data":123}})
+    context "when there is a reponse" do
+      before_each do
+        WebMock.stub(:get, "https://test.com").
+          to_return(body: %{{"data":123}})
+      end
+
+      it "fetches a response and parses it" do
+        value = Fetcher(ExampleEntity).new.fetch("https://test.com")
+        value.data.should eq 123
+      end
+
+      it "returns the parsed response as the generic type" do
+        value = Fetcher(ExampleEntity).new.fetch("https://test.com")
+        value.should be_a ExampleEntity
+      end
     end
 
-    it "fetches a response and parses it" do
-      value = Fetcher(ExampleEntity).new.fetch("https://test.com")
-      value.data.should eq 123
-    end
+    context "when the request fails" do
+      before_each do
+        WebMock.stub(:get, "https://test.com").
+          to_return(status: 401, body: "Forbidden")
+      end
 
-    it "returns the parsed response as the generic type" do
-      value = Fetcher(ExampleEntity).new.fetch("https://test.com")
-      value.should be_a ExampleEntity
+      it "raises an exception" do
+        expect_raises(Fetcher::FetchFailed) do
+          Fetcher(ExampleEntity).new.fetch("https://test.com")
+        end
+      end
     end
   end
 end
--- backend/src/fetcher.cr
+++ backend/src/fetcher.cr
@@ -1,6 +1,20 @@
 class Fetcher(T)
+  class FetchFailed < Exception; end
+
   def fetch(url : String) : T
     response = HTTP::Client.get(url)
-    T.from_json(response.body)
+    handle_failure(response) unless response.success?
+
+    begin
+      T.from_json(response.body)
+    rescue e : JSON::MappingError
+      puts "Failed to parse body as #{T.class}: #{response.body}"
+      raise e
+    end
+  end
+
+  private def handle_failure(response : HTTP::Client::Response) : Void
+    puts "Failed to run request: #{response.body}"
+    raise FetchFailed.new("boo") unless response.success?
   end
 end
